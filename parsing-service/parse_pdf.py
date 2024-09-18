68aa2d6bd15b4f42ac5cafff311a34ad584d7326
Den Tomorrow <whattomson@gmail.com>
2024-09-18 13:02:43 +0300

    Код для поиска элементов страницы

--- parsing-service/parse_pdf.py
+++ parsing-service/parse_pdf.py
@@ -17,7 +17,23 @@ from pdf2image import convert_from_path
 
 
 def text_from_pdf(pdf_path: str) -> None:
-    pass
+    pdfFileObj = open(pdf_path, 'rb')
+    pdfReaded = PyPDF2.PdfReader(pdfFileObj)
+    
+    text_per_page = {}
+    
+    # Извлекаем страницы из PDF
+    for page_num, page in tqdm(enumerate(extract_pages(pdf_path)), 'Pages', position=2):
+        pageObj = pdfReaded.pages[page_num]
+        page_text, line_format = [], []
+        
+        
+        # Находим все элементы
+        page_elements = [(element.y1, element) for element in page._objs]
+        # Сортируем все элементы по порядку нахождения на странице
+        page_elements.sort(key=lambda a: a[0], reverse=True)
+        
+        print(page_elements)
 
 
 def create_txt_files() -> None:
@@ -34,7 +50,7 @@ def _text_extraction() -> None:
 
 
 def main() -> None:
-    pass
+    text_from_pdf(pdf_path='.\\data\\pdf\\9)KKM-2022Razdel5Bespozvonochnie-4chasts426-484.pdf')
 
 
 if __name__ == "__main__":
