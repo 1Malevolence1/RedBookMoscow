4a58c9d303ae46b3e22e608178ab909ad77847a2
Mr.D <danil36rusdanil@mial.ru>
2024-09-18 11:37:09 +0300

    Написал первые dto

---
+++ admin-panel-service/src/main/java/com/example/admin_panel_service/dto/RequestDtoRedBookEntry.java
@@ -0,0 +1,24 @@
+package com.example.admin_panel_service.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RequestDtoRedBookEntry {
+    private String name;
+    private String latinName;
+    private String division;
+    private String family;
+    private String Status;
+    private String distribution;
+    private String inhabitat;
+    private String mitigatingFactor;
+    private String ProtectionMeasuresTaken;
+    private String ChangesINinTheStatusOfTheSpecies;
+    private String neededConservationcAtions;
+
+}
