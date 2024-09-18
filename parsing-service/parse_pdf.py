e1cd2513556e8de38586d8c826ab2dc2264c074f
Den Tomorrow <whattomson@gmail.com>
2024-09-19 00:44:52 +0300

    Сделал функцию для вырезки и сохранения фото в папку

--- parsing-service/parse_pdf.py
+++ parsing-service/parse_pdf.py
@@ -1,3 +1,4 @@
+import imaplib
 import os
 import re
 import time
@@ -24,19 +25,17 @@ import pytesseract
 from deep_translator import GoogleTranslator
 
 
-def images_from_pdf(pdf_path: str) -> None:
+def get_images_from_pdf(pdf_path: str) -> None:
     pdfFileObj = open(pdf_path, 'rb')
     pdfReaded = PyPDF2.PdfReader(pdfFileObj)
     
     # Извлекаем страницы из PDF
     for page_num, page in tqdm(enumerate(extract_pages(pdf_path)), 'Pages', position=2):
         
-        if page_num > 1:
+        if page_num > 0:
             break
         
         pageObj = pdfReaded.pages[page_num]
-        page_text, line_format = [], []
-        page_content = []
         
         page_elements = [(element.y1, element) for element in page._objs]
         page_elements.sort(key=lambda a: a[0], reverse=True)
@@ -46,109 +45,98 @@ def images_from_pdf(pdf_path: str) -> None:
             # Положение верхнего края элемента в PDF, Элемент структуры страницы
             pos, element = component[0], component[1] 
 
-            # Проверяем, является ли элемент текстовым
-            if isinstance(element, LTTextContainer):
-                line_text, format_per_line = _text_extraction(element=element)
-                # page_text.append(line_text)
-                # line_format.append(format_per_line)
-                # name_key = _get_name_key(line_text, format_per_line)
-                # if name_key:
-                    # print(name_key)
-                page_text.append(line_text)
+            if isinstance(element, LTFigure):
+                # Вырезаем изображение из PDF
+                cropped_image_path = _crop_image(element, pageObj)
+                image_path = _convert_to_images(cropped_image_path, path_output_with_name=f'./data/tmp/{page_num}_{i}')
+                
+                
 
 
+def _delete_unsuitable_images(input_folder, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-def _get_name_key(line_text, format_per_line):
-    name_key = line_text[line_text.find('\x1f') + 1: line_text.find('.')] \
-                                                            .strip() \
-                                                            .replace("-", '') \
-                                                            .replace('\n', ' ') \
-                                                            .replace('\t', ' ')
-    bold_check = any(["Bold" in str(format) for format in format_per_line]) 
-    italic_check = not any(["Italic" in str(format) for format in format_per_line])
-    upper_check = any([name.isupper() for name in name_key.split()])
-    if upper_check and bold_check:
-        name_key = _get_valid_name_key(name_key, line_text)
-        name_key = name_key.strip()
-        if name_key != "Автор" and name_key != 'Авторы':
-            name_key = name_key.replace('Отряд', "| Отряд").replace("Семейство", "| Семейство")
-            name_key = name_key.split('|')
-            try:
-                name, latin_name = _split_ru_en_string(name_key[0].strip())
-                division = name_key[1]
-                family = name_key[2]
-                return '\n'.join([name.strip(), latin_name.strip(), division.strip(), family.strip()])
-            except Exception as e:
-                return name_key.strip(), 'exception'
-            
-        else:
-            return name_key.strip()
-    else:
-        if bold_check and italic_check:
-            name_key = _get_valid_name_key(name_key, line_text)
-            return name_key.strip()
-    return None
+    for filename in os.listdir(input_folder):
+        if filename.endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(input_folder, filename).replace('\\', '/')
+            with Image.open(image_path) as img:
+                if _has_text(img):
+                    os.remove(image_path)
+                elif _is_horizontal(img):
+                    os.remove(image_path)
+                elif _is_many_white(image_path):
+                    os.remove(image_path)
 
 
-def _split_ru_en_string(text):
-    match = re.search(r'[A-Za-z]', text)
-    if match:
-        return text[: match.start()], text[match.start(): ]
+def _is_many_white(image_path):
+    img = Image.open(image_path)
+    img = img.convert('RGB')
+
+    width, height = img.size
+    white_pixel_count = 0
+
+    for x in range(width):
+        for y in range(height):
+            r, g, b = img.getpixel((x, y))
+            if (r, g, b) == (255, 255, 255):
+                white_pixel_count += 1
+
+    return white_pixel_count / (width * height) > 0.8
 
 
-def _get_valid_name_key(name_key,line_text):
-    
-    translations = {
-        "Имя": "Name",
-        "Латинское имя": "Latin Name",
-        "Отряд": "Division",
-        "Семейство": "Family",
-        "Статус": "Status",
-        "Распространение": "Distribution",
-        "Численность": "Inhabitat",
-        "Особенности обитания": "Habitat Features",
-        "Лимитирующие факторы": "Mitigating factors",
-        "Принятые меры охраны": "Protection measures taken",
-        "Изменение состояния вида": "Changes in the status of the species",
-        "Необходимые мероприятия по сохранению вида": "Needed conservation actions",
-        "Источники информации": "Sources of Information",
-        "Автор": "Authors",
-        "Авторы": "Authors"
-    }
-    if ':' in name_key:
-        name_key = name_key[: name_key.find(':')]
-    return name_key
+def _is_horizontal(image, max_width_to_height_ratio=3):
+    """Проверяет, является ли изображение слишком горизонтальным."""
+    width, height = image.size
+    return width / height > max_width_to_height_ratio
 
 
+def _has_text(image):
+    return bool(pytesseract.image_to_string(image).strip())
 
-def _text_extraction(element) -> None:
-    line_text = element.get_text()
-    line_formats = []
 
-    for text_line in element:
-        if isinstance(text_line, LTTextContainer):
-            # Итеративно обходим каждый символ в строке текста
-            for character in text_line:
-                if isinstance(character, LTChar):
-                    # Добавляем к символу название шрифта
-                    line_formats.append(character.fontname)
-                    # Добавляем к символу размер шрифта
-                    line_formats.append(character.size)
+# Функция для вырезания элементов изображений из PDF
+def _crop_image(element, pageObj, path_output_with_name: str ='./data/tmp/cropped_image'):
+    # Получаем координаты для вырезания изображения из PDF
+    [image_left, image_top, image_right, image_bottom] = [element.x0,element.y0,element.x1,element.y1] 
+    # Обрезаем страницу по координатам (left, bottom, right, top)
+    pageObj.mediabox.lower_left = (image_left, image_bottom)
+    pageObj.mediabox.upper_right = (image_right, image_top)
+    # Сохраняем обрезанную страницу в новый PDF
+    cropped_pdf_writer = PyPDF2.PdfWriter()
+    cropped_pdf_writer.add_page(pageObj)
+    # Сохраняем обрезанный PDF в новый файл
+    with open(f'{path_output_with_name}.pdf', 'wb') as cropped_pdf_file:
+        cropped_pdf_writer.write(cropped_pdf_file)
+    return f'{path_output_with_name}.pdf'
 
-    # Формат может на будущее пригодиться
-    format_per_line = list(set(line_formats))    
-    return (line_text, format_per_line)
+
+# Функция для преобразования PDF в изображения
+def _convert_to_images(input_file: str, path_output_with_name: str ='./data/tmp/PDF_image'):
+    poppler_path = r"C:\poppler-24.02.0\Library\bin"
+    image = convert_from_path(input_file, poppler_path=poppler_path)[0]
+
+    output_path = f"{path_output_with_name}.png"
+    image.save(output_path, "PNG")
+    os.remove('./data/tmp/cropped_image.pdf')
+    return f"{output_path}.png"
 
 
 def extract_text_from_image(image):
     """Извлекает текст из изображения с помощью Pytesseract."""
     return pytesseract.image_to_string(image, lang='rus+eng')
 
-def save_text_to_file(text, output_file):
+def save_text_to_file(input_file, output_file):
+    images = convert_from_path(input_file)
+    full_text = ""
+    
+    for i, image in tqdm(enumerate(images)):
+        text = extract_text_from_image(image)
+        full_text += text + '\n'
+
     """Сохраняет извлеченный текст в файл."""
     with open(output_file, 'w', encoding='utf-8') as f:
-        f.write(text)
-
+        f.write(full_text)
 
 
 def main() -> None:
@@ -160,14 +148,12 @@ def main() -> None:
     # except FileNotFoundError:
         # text_from_pdf(pdf_path=path2)
     
-    images = convert_from_path(path1)
-    full_text = ""
     
-    for i, image in enumerate(images):
-        text = extract_text_from_image(image)
-        full_text += text + '\n'
+    # save_text_to_file(path2, path2.replace('/pdf', '/txt').replace('.pdf', '.txt'))
+    get_images_from_pdf(pdf_path=path2)
+    _delete_unsuitable_images(input_folder='./data/tmp', output_folder='./data/tmp')
 
-    save_text_to_file(full_text, './data/tmp/test_txt2.txt')
+    
 
 
 if __name__ == "__main__":
