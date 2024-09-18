9b11f77b661dbe2b91ac2b48eaa88ed52d97ed79
Mr.D <danil36rusdanil@mial.ru>
2024-09-18 12:41:56 +0300

    Исправил ошибку

--- user-service/src/main/java/com/example/user_service/config/uri/PathUriController.java
+++ user-service/src/main/java/com/example/user_service/config/uri/PathUriController.java
@@ -6,5 +6,5 @@ public final class PathUriController {
 
     // @TODO добавить путь
     public static String GET_LIST_RED_BOK_ENTRY_DATA_BASE = "";
-
+    public static String GET__RED_BOK_ENTRY_DATA_BASE = "%d";
 }
