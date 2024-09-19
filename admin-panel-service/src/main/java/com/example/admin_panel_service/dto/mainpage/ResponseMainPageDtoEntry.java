4eadf7b0d3a63d831830660f370eeff00d43e952
Mr.D <danil36rusdanil@mial.ru>
2024-09-19 04:11:25 +0300

    save

--- admin-panel-service/src/main/java/com/example/admin_panel_service/dto/mainpage/ResponseMainPageDtoEntry.java
+++ admin-panel-service/src/main/java/com/example/admin_panel_service/dto/mainpage/ResponseMainPageDtoEntry.java
@@ -12,21 +12,6 @@ public class ResponseMainPageDtoEntry {
         private Long id;
         private String name;
         private String latinName;
-        private String division;
-        private String Family;
-
-        private String status;
-        private String distribution;
-        private String inHabitat;
-        private String habitatFeatures;
-
-        private String mitigatingFactors;
-        private String protectionMeasuresTaken;
-        private String changesInStatusOfSpecies;
-        private String neededConservationActions;
-
-        private String sourcesOfInformation;
-        private String authors;
         private String data;
         private String view;
 }
